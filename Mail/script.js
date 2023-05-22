d3ed46d9e69ce74267f263e838c92f6c2ebc4e15
SteCard <stefanocardillo14@gmail.com>
2023-05-22 23:50:29 +0200

    Mail update

---
+++ Mail/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// ARRAY EMAIL CON ACCESSO
+let mail_list = ["MartyMcFly@gmail.com", "FrodoBaggins@gmail.com", "MarkLenders@gmail.com", "McLovin@gmail.com"];
+
+// PROMPT EMAIL
+let email = prompt('Inserire Email');
+
+// VARIABILE DI CONTROLLO - FLAG
+let flag = false;
+
+// CICLO FOR ARRAY MAIL LIST
+for (let i = 0; i < mail_list.length; i++){
+
+    // CONFRONTO EMAIL ARRAY CON EMAIL INSERITA DALL'UTENTE
+    if (mail_list[i].toLowerCase() === email.toLowerCase()){
+        
+        flag = true;
+    }
+}
+
+// MESSAGGIO ESITO CONTROLLO EMAIL
+if(flag === true){
+
+    console.log('Mail valida')
+    alert("L'email inseritaha superato la verifica di accesso, è possibile proseguire.");
+
+} 
+else{ 
+
+    console.log('Mail NON valida')
+    alert("L'email inserita NON ha superato la verifica per accedere.");
+}
